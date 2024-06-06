a546642addbb1fcf6586d65bbf70e5f08d52801a
MalaikaSaleem111 <malaikamsaleem@email.com>
2024-06-06 13:16:49 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env nodenp
+import inquirer from "inquirer";
+// define the list of currecies and their exchange rates
+let exchange_rates: any = {
+    "USD" : 1,//base currency
+    "EUR" :0.92,
+    "JYP" : 157.26,
+    "INR" : 83.44,
+    "PKR" :278.10,
+};
+let user_answer = await inquirer.prompt([{
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "JYP", "INR", "PKR"]
+},
+{
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "JYP", "INR", "PKR"]
+},
+{
+    name: "amount",
+    message: "Enter your amount",
+    type: "number"
+    }
+
+]);
+
+let fromAmount = exchange_rates[user_answer.from];
+let toAmount = exchange_rates[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount= amount / fromAmount;//convert into base currency
+let covertedAmounted = baseAmount*toAmount;
+console.log(covertedAmounted.toFixed(2));
